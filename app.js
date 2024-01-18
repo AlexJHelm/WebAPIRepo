79fec646fe6cd13271ea97936323c6455325fb07
AlexJHelm <ajhelm@email.neit.edu>
2024-01-18 14:50:52 -0500

    Added Static Website

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var path = require('path');
+var mongoose = require('mongoose');
+
+//connect to mongodb via mongoose
+mongoose.connect("mongodb://0.0.0.0:27017/WebAPI",{
+}).then(function(){
+    console.log("Connected to MongoDB Database");
+}).catch(function(err){
+    console.log(err);
+});
 
 //var num = 6*6;
 
@@ -16,14 +25,32 @@ app.get('/', function(req,res){
     res.sendFile(path.join(__dirname+"/pages/index.html"))
 });
 
-app.get('/about', function(req,res){
+app.get('/login', function(req,res){
     //res.send("here would be the page from the route");
-    res.sendFile(path.join(__dirname+"/pages/about.html"))
+    res.sendFile(path.join(__dirname+"/pages/login.html"))
 });
 
-app.get('/contact', function(req,res){
+app.get('/add', function(req,res){
     //res.send("here would be the page from the route");
-    res.sendFile(path.join(__dirname+"/pages/contact.html"))
+    res.sendFile(path.join(__dirname+"/pages/add.html"))
+});
+
+var Schema = mongoose.Schema;
+
+var GameData = new Schema({
+    gamename:{
+        type:String
+    }
+    //gameName:String,
+    //gameStudio:String
+});
+
+var gameModel = mongoose.model("gameModel", GameData);
+
+app.get("/getdata", function(req, res){
+    gameModel.find({}).then(function(games){
+        res.json({gameData});
+    });
 });
 
 app.listen(3000, function(){
